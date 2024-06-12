ee509c8bb99552e3fd75f44eedb6ece3143cd76f
Mohit <89613841+MohitBallikar@users.noreply.github.com>
2024-06-11 20:21:06 -0400

    Updated main.c with comments and watched the full video
    need to figure out the pin assignment for this board, so oscilloscope can be used to test timer frequency
    
    from here can fine-tune the clock scale if it is not the correct 1.25 uSeconds
    
    The logic should be good, though the define statements are elastic for the number of LEDs needed to be connected in series
    
    Finally special animations are included in the latter part of the video
    
    https://youtu.be/-3VKkTSAytM

--- Code/STM32/main.c
+++ Code/STM32/main.c
@@ -1,29 +1,140 @@
-
+/* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
-#define MAX_LED 6 //will be used to define the maximum arranged LEDS in a cascade
-#define USE_BRIGHTNESS 1
+#include "math.h"
 
-uint8_t LED_Data[MAX_LED][4];
-uint8_t LED_Mod[MAX_LED][4];
-
-UART_HandleTypeDef hlpuart1;
-
-SPI_HandleTypeDef hspi2;
-
+/* Private variables ---------------------------------------------------------*/
 TIM_HandleTypeDef htim1;
 DMA_HandleTypeDef hdma_tim1_ch1;
 
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
-static void MX_LPUART1_UART_Init(void);
 static void MX_TIM1_Init(void);
-static void MX_SPI2_Init(void);
+/* USER CODE BEGIN PFP */
 
+#define MAX_LED 8 //goober number should switch to the value in series
+#define USE_BRIGHTNESS 1 //binary setting for on or off
+
+//4x4 matrix to represent the used LEDs may need to expand this number if the LED_MAX is increased
+uint8_t LED_Data[MAX_LED][4]; //for color (i think)
+uint8_t LED_Mod[MAX_LED][4]; //for individual brightness
+
+int datasentflag = 0; //initialization for the DMA start
+
+void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim)
+{
+	HAL_TIM_PWM_Stop_DMA(&htim1, TIM_CHANNEL_1);
+	datasentflag = 1; //DMA set for after DMA is completed
+}
+
+void Set_LED (int LEDnum, int Red, int Green, int Blue)
+{
+	LED_Data[LEDnum][0] = LEDnum;
+	LED_Data[LEDnum][1] = Green;
+	LED_Data[LEDnum][2] = Red;
+	LED_Data[LEDnum][3] = Blue;
+}
+
+#define PI 3.14159265
+
+void Set_Brightness (int brightness) //sets brightness for the LED(s), self explanatory
+//remember that the scale goes from 0-45 | examples- 255 = 100%; 127 = 50%; 63 = 25%; (brightness ratios for the full brightness amount
+{
+#if USE_BRIGHTNESS
+
+	if (brightness > 45) brightness = 45;
+	for (int i = 0; i < MAX_LED; i++)
+	{
+		LED_Mod[i][0] = LED_Data[i][0];
+		for (int j = 1; j < 4; j++)
+		{
+			float angle = 90 - brightness; //in degrees
+			angle = angle * PI / 180; //in radians
+			LED_Mod[i][j] = (LED_Data[i][j])/(tan(angle)); //this shit it done to change the linearity, not my solution
+		}
+	}
+#endif
+}
+
+uint16_t pwmData[(24*MAX_LED) + 50]; //the 24 bits is per LED and any more is shifted to the next one
+//you can store 24 times the LEDs plus a flat 50 values
+//the extra 50 is the 50 uSecond reset that is used in-between transmissions
+
+//recall that the pwm width is 1.25 uSeconds
+void WS2812_Send (void) //function used to send the values to the pwm dma
+{
+	uint32_t indx = 0;
+	uint32_t color; //32 bit variable used for the 24 bit color data (the cringe color data stream)
+
+	for (int i = 0; i < MAX_LED; i++) //performs the operation for every LED individually
+	{
+		color = ((LED_Mod[i][1]<<16) /*Green*/ | (LED_Mod[i][2]<<8) /*Red*/ | LED_Mod[i][3]); /*Blue*/
+		//green code (value) in the second column, red in the third column, blue in the fourth column
+		//a 16 bit shift is done for green, 8 bit for red, and no shift for blue
+		for (int i = 23; i >= 0; i--)
+		{
+			if (color&(1<<i)) //now individual bits in the color variable are checked here, for linearity i'm guessing
+			{
+				//if MSB (most sig bit) is 1, then 60 is stored in LSB (least sig bit)
+				pwmData[indx] = 60; // 2/3rds of the duty cycle
+			}
+
+			else pwmData[indx] = 30; // 1/3rd of the duty cycle
+
+			indx++;
+		}
+	}
+
+	for (int i = 0; i < 50; i++) //need to add 50 zeros lol, check the data sheet. Unironically the 60 uSecond reset code
+	{
+		pwmData[indx] = 0;
+		indx++;
+	}
+	//once LED values are stored then it is iterated through the matrix
+
+	HAL_TIM_PWM_Start_DMA(&htim1, TIM_CHANNEL_1, (uint32_t *)pwmData, indx); //after all the storing is finished we send the data to the dma
+	while(!datasentflag){}; //after sending all the data, we wait for the flag to set; indicates that DMA is stopped and another set can now be sent
+	datasentflag = 0;
+}
+
+//void send (int Green, int Red, int Blue)
+//{
+//	uint32_t data = (Green<<16) | (Red<<8) | Blue;
+//
+//	for (int i = 23; i>= 0; i--)
+//	{
+//		if (data&(1<<i)) pwmData[i] = 60; //duty cycle for transmitting a one
+//
+//		else pwmData[i] = 30; //duty cycle for transmitting a 0
+//	}
+//
+//	HAL_TIM_PWM_Start_DMA(&htim1, TIM_CHANNEL_1, (uint32_t *)pwmData, 24); //starts the pulse width modulation
+//
+//}
+
+
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
 int main(void)
 {
+
   /* USER CODE BEGIN 1 */
 
   /* USER CODE END 1 */
@@ -47,115 +158,27 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_DMA_Init();
-  MX_LPUART1_UART_Init();
   MX_TIM1_Init();
-  MX_SPI2_Init();
   /* USER CODE BEGIN 2 */
 
-  Set_LED(0, 255, 0, 0);
-  Set_LED(1, 0, 255, 0);
-  Set_LED(2, 0, 0, 255);
+  Set_LED(0, 255, 0, 0); //purely red
+  Set_LED(1, 0, 255, 0); //purely green
+  Set_LED(2, 0, 0, 255); //purely blue
 
-  Set_LED(3, 46, 89, 128);
+  Set_Brightness(45); //45 is full bright, recall
+  WS2812_Send();
 
-  Set_LED(4, 156, 233, 100);
-  Set_LED(5, 102, 0, 235);
-  Set_LED(6, 47, 38, 77);
+  /* USER CODE END 2 */
 
-  Set_LED(7, 255, 200, 0);
+  /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  for (int i=0; i<46; i++)
-	 	  {
-	 		  Set_Brightness(i);
-	 		  WS2812_Send();
-	 		  HAL_Delay (50);
-	 	  }
+    /* USER CODE END WHILE */
 
-	 	  for (int i=45; i>=0; i--)
-	 	  {
-	 		  Set_Brightness(i);
-	 		  WS2812_Send();
-	 		  HAL_Delay (50);
-	 	  }
+    /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
-}
-
-uint16_t pwmData[(24*MAX_LED)+50];
-
-void WS2812_Send (void)
-{
-	uint32_t indx=0;
-	uint32_t color;
-
-
-	for (int i= 0; i<MAX_LED; i++)
-	{
-#if USE_BRIGHTNESS
-		color = ((LED_Mod[i][1]<<16) | (LED_Mod[i][2]<<8) | (LED_Mod[i][3]));
-#else
-		color = ((LED_Data[i][1]<<16) | (LED_Data[i][2]<<8) | (LED_Data[i][3]));
-#endif
-
-		for (int i=23; i>=0; i--)
-		{
-			if (color&(1<<i))
-			{
-				pwmData[indx] = 60;  // 2/3 of 90
-			}
-
-			else pwmData[indx] = 30;  // 1/3 of 90
-
-			indx++;
-		}
-
-	}
-
-	for (int i=0; i<50; i++)
-	{
-		pwmData[indx] = 0;
-		indx++;
-	}
-
-	HAL_TIM_PWM_Start_DMA(&htim1, TIM_CHANNEL_1, (uint32_t *)pwmData, indx);
-	while (!datasentflag){};
-	datasentflag = 0;
-}
-
-void Set_LED (int LEDnum, int Red, int Green, int Blue)
-{
-	LED_Data[LEDnum][0] = LEDnum;
-	LED_Data[LEDnum][1] = Green;
-	LED_Data[LEDnum][2] = Red;
-	LED_Data[LEDnum][3] = Blue;
-
-}
-
-#define PI 3.14159265
-
-void Set_Brightness (int brightness) // 0-45
-{
-#if USE_BRIGHTNESS
-	if (brightness > 45) brightness = 45;
-	for (int i = 0; i < MAX_LED; i++)
-	{
-		LED_Mod[i][0] = LED_Data[i][0];
-		for (int j = 1; j < 4; j++)
-		{
-			float angle = 90 - brightness; //degrees
-			angle = angle * PI / 180 //in radians
-			LED_Mod[i][j] = (LED_Data[i][j])/(tan(angle));
-		}
-	}
-#endif
-}
-
-void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim)
-{
-	HAL_TIM_PWM_Stop_DMA(&htim1, TIM_CHANNEL_1);
-	datasentflag=1;
 }
 
 /**
@@ -174,13 +197,12 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV1;
-  RCC_OscInitStruct.PLL.PLLN = 9;
+  RCC_OscInitStruct.PLL.PLLN = 18;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
   RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
@@ -196,99 +218,12 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
     Error_Handler();
   }
-}
-
-/**
-  * @brief LPUART1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_LPUART1_UART_Init(void)
-{
-
-  /* USER CODE BEGIN LPUART1_Init 0 */
-
-  /* USER CODE END LPUART1_Init 0 */
-
-  /* USER CODE BEGIN LPUART1_Init 1 */
-
-  /* USER CODE END LPUART1_Init 1 */
-  hlpuart1.Instance = LPUART1;
-  hlpuart1.Init.BaudRate = 115200;
-  hlpuart1.Init.WordLength = UART_WORDLENGTH_8B;
-  hlpuart1.Init.StopBits = UART_STOPBITS_1;
-  hlpuart1.Init.Parity = UART_PARITY_NONE;
-  hlpuart1.Init.Mode = UART_MODE_TX_RX;
-  hlpuart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  hlpuart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
-  hlpuart1.Init.ClockPrescaler = UART_PRESCALER_DIV1;
-  hlpuart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-  if (HAL_UART_Init(&hlpuart1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  if (HAL_UARTEx_SetTxFifoThreshold(&hlpuart1, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  if (HAL_UARTEx_SetRxFifoThreshold(&hlpuart1, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  if (HAL_UARTEx_DisableFifoMode(&hlpuart1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN LPUART1_Init 2 */
-
-  /* USER CODE END LPUART1_Init 2 */
-
-}
-
-/**
-  * @brief SPI2 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_SPI2_Init(void)
-{
-
-  /* USER CODE BEGIN SPI2_Init 0 */
-
-  /* USER CODE END SPI2_Init 0 */
-
-  /* USER CODE BEGIN SPI2_Init 1 */
-
-  /* USER CODE END SPI2_Init 1 */
-  /* SPI2 parameter configuration*/
-  hspi2.Instance = SPI2;
-  hspi2.Init.Mode = SPI_MODE_MASTER;
-  hspi2.Init.Direction = SPI_DIRECTION_2LINES;
-  hspi2.Init.DataSize = SPI_DATASIZE_4BIT;
-  hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
-  hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
-  hspi2.Init.NSS = SPI_NSS_SOFT;
-  hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
-  hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
-  hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
-  hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
-  hspi2.Init.CRCPolynomial = 7;
-  hspi2.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
-  hspi2.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
-  if (HAL_SPI_Init(&hspi2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN SPI2_Init 2 */
-
-  /* USER CODE END SPI2_Init 2 */
-
 }
 
 /**
@@ -303,6 +238,7 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 0 */
 
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
   TIM_MasterConfigTypeDef sMasterConfig = {0};
   TIM_OC_InitTypeDef sConfigOC = {0};
   TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
@@ -313,10 +249,19 @@ static void MX_TIM1_Init(void)
   htim1.Instance = TIM1;
   htim1.Init.Prescaler = 0;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 71;
+  htim1.Init.Period = 89;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
   if (HAL_TIM_PWM_Init(&htim1) != HAL_OK)
   {
     Error_Handler();
@@ -371,12 +316,12 @@ static void MX_DMA_Init(void)
 
   /* DMA controller clock enable */
   __HAL_RCC_DMAMUX1_CLK_ENABLE();
-  __HAL_RCC_DMA1_CLK_ENABLE();
+  __HAL_RCC_DMA2_CLK_ENABLE();
 
   /* DMA interrupt init */
-  /* DMA1_Channel1_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(DMA1_Channel1_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(DMA1_Channel1_IRQn);
+  /* DMA2_Channel1_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA2_Channel1_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA2_Channel1_IRQn);
 
 }
 
@@ -387,31 +332,12 @@ static void MX_DMA_Init(void)
   */
 static void MX_GPIO_Init(void)
 {
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
 /* USER CODE BEGIN MX_GPIO_Init_1 */
 /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOF_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin : B1_Pin */
-  GPIO_InitStruct.Pin = B1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : LD2_Pin */
-  GPIO_InitStruct.Pin = LD2_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+  __HAL_RCC_GPIOC_CLK_ENABLE();
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
 /* USER CODE END MX_GPIO_Init_2 */
