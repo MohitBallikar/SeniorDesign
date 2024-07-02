a3b271264f7ddb5916ce025bbb43990b044da337
Mohit <89613841+MohitBallikar@users.noreply.github.com>
2024-07-01 23:01:28 -0400

    Update subprocesstest.py; included screen flip commands to be used later on in the engine for the two different views that will be alternated every two turns

--- Code/Rasp-Pi/subprocesstest.py
+++ Code/Rasp-Pi/subprocesstest.py
@@ -6,9 +6,14 @@ print("we are here")
 #command = " pwd"
 #command = "cd ~/Pn532-nfc-hat-code/raspberrypi/python | python3 example_get_uid.py"
 #command = "cd ~/Pn532-nfc-hat-code/raspberrypi/python | ls"
-command = "python3 example_get_uid.py"
+NFC_GET_UID = "python3 example_get_uid.py"
+normal_flip = "wlr-randr --output DSI-1 --transform normal"
+invert_flip = "wlr-randr --output DSI-1 --transform 180"
 
-result = subprocess.run(command, shell=True, capture_output=False, text=True)
+result = subprocess.run(NFC_GET_UID, shell=True, capture_output=False, text=True)
+
+#every two turns have the display flip
+#
 
 print("Output:")
 print(result.stdout)
