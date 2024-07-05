92f92401fc6a9000e2bac33f3632f9e9e627d586
Mohit <89613841+MohitBallikar@users.noreply.github.com>
2024-07-04 20:28:02 -0400

    Update GUI.py

--- Code/Rasp-Pi/GUI.py
+++ Code/Rasp-Pi/GUI.py
@@ -1,5 +1,5 @@
 import PySimpleGUI as sg
-from subprocesstest import GameEngine 
+#from GameEngine import Engine
 ##screen selection button is broken (dont need for final, but for testing can be nice)
 
 rubies = None
@@ -13,30 +13,38 @@ gold_str = str(gold)
 # ----------- Create the 3 layouts this Window will display -----------
 layout1 = [[sg.Text('Turn Selection', font=('Helvetica', 32), size=(500,1), justification='center')],
            [sg.Text('')],[sg.Text('')],[sg.Text('')],
-           [sg.Button('Attack', button_color= ('#ffffff', '#b51d09'), font=('Helvetica', 64)), sg.Text('\t\t\t'), sg.Button('Growth', button_color=('#ffffff','#0b8a2b'), font=('Helvetica', 64))],
+           [sg.Button('Attack', button_color= ('#ffffff', '#b51d09'), font=('Helvetica', 60)), sg.Text('\t\t\t'), sg.Button('Growth', button_color=('#ffffff','#0b8a2b'), font=('Helvetica', 60))],
            [sg.Text('')], [sg.Text('')], [sg.Text('')],[sg.Text('')]]
 
 #work on these
 layout2 = [[sg.Text('Attack - Move your Desired Pieces', font=('Helvetica', 32), size=(500,1), justification='center')],
            [sg.Text('')],[sg.Text('')],[sg.Text('')],
-           [sg.Text('\t\t\t\t\t'),sg.Button('Confirm', font=('Helvetica', 32))],
+           [sg.Text('\t\t\t\t  '),sg.Button('Confirm', font=('Helvetica', 32))],
            [sg.Text('')],
            [sg.Text('')]]
 
-layout3 = [[sg.Text('This is layout 3: Growth - Choose a Resource')],[sg.Text('Rubies: ' + rubies_str)],[sg.Text('Sapphire: ' + sapphires_str)],[sg.Text('Gold: ' + gold_str)],
-           [sg.Button('Ruby'), sg.Button('Sapphire'), sg.Button('Emerald')]]
+layout3 = [[sg.Text('Growth - Choose a Resource', font=('Helvetica', 32), size=(500,1), justification='center')],
+           [sg.Text('\tRubies: ' + rubies_str + '\tSapphires: ' + sapphires_str + '\tGold: ' + gold_str, font=('Helvetica', 18))],
+           [sg.Text('')],
+           [sg.Text('')],
+           [sg.Text('\t'), sg.Button('Ruby', font=('Helvetica', 32)), sg.Text('\t'), sg.Button('Sapphire', font=('Helvetica', 32)), sg.Text('\t'), sg.Button('Gold', font=('Helvetica', 32))],
+           [sg.Text('')],
+           [sg.Text('')],
+           [sg.Text('')]]
 
-layout4 = [[sg.Text('This is layout 4: Growth - Choose a card type to purchase')],
-            [sg.Button('Attack', font=('Helvetica', 64)), sg.Button('Growth', font=('Helvetica', 64))]]
+layout4 = [[sg.Text('Growth - Choose a Card Type to Purchase', font=('Helvetica', 28), size=(500,1), justification='center')],
+           [sg.Text('')],[sg.Text('')],[sg.Text('')],
+           [sg.Button('Attack', button_color= ('#ffffff', '#b51d09'), font=('Helvetica', 60)), sg.Text('\t\t\t'), sg.Button('Growth', button_color=('#ffffff','#0b8a2b'), font=('Helvetica', 60))],
+           [sg.Text('')], [sg.Text('')], [sg.Text('')],[sg.Text('')]]
 
 # ----------- Create actual layout using Columns and a row of Buttons
 layout = [[sg.Column(layout1, key='-COL1-'), sg.Column(layout2, visible=False, key='-COL2-'), sg.Column(layout3, visible=False, key='-COL3-'), sg.Column(layout4, visible=False, key='-COL4-')],
-          [sg.Text('\t\t\t'), sg.Button('Next Screen', font=('Helvetica', 52))]]
+          [sg.Text('\t\t\t'), sg.Button('Next Screen', font=('Helvetica', 40))]]
 
 
 window = sg.Window(' ', layout, size=(800, 480), text_justification='center')
 
-game_engine = GameEngine()
+#game_engine = GameEngine()
 
 layout = 1  # The currently visible layout
 while True:
