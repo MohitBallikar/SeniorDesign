1d428acef3fa1ef06a880ca0238654c95cf667aa
Mohit <89613841+MohitBallikar@users.noreply.github.com>
2024-07-08 20:44:22 -0400

    Update uart_reader.py, should be handled better as an object/class

--- Code/Rasp-Pi/Packages/uart_reader.py
+++ Code/Rasp-Pi/Packages/uart_reader.py
@@ -1,12 +1,17 @@
-# uart_reader.py
-
 import serial
-
+#USB0 is the commumication port
+#timeout is in seconds; ie 3 seconds here
 class UARTReader:
-    def __init__(self, port, baudrate=9600):
-        self.ser = serial.Serial(port, baudrate)
+    def __init__(self, port = "/dev/ttyUSB0", baudrate=115200, timeout=1.5):
+        self.port = serial.Serial(port, baudrate=baudrate, timeout=timeout)
+    
+    def send_data(self, data):
+        d = bytes(data, 'utf-8')
+        self.port.write(d)
+        rcv = self.port.read(16)
+        return rcv
+##data = "SETLED 4 G"
+    def close(self):
+        self.port.close()
+#LED data is sent as "SETLED [LED_NUM] [COLOR]" with color being R, G, B, Y 
 
-    def read_line(self):
-        if self.ser.in_waiting > 0:
-            return self.ser.readline().decode('utf-8').strip()
-        return None
