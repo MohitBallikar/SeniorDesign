cd4e6c09cc7038a4963e2f1ac367a4eccaa3f46f
Mohit <89613841+MohitBallikar@users.noreply.github.com>
2024-07-08 22:37:08 -0400

    Update uart_reader.py; should be a bit better

--- Code/Rasp-Pi/Packages/uart_reader.py
+++ Code/Rasp-Pi/Packages/uart_reader.py
@@ -5,13 +5,16 @@ class UARTReader:
     def __init__(self, port = "/dev/ttyUSB0", baudrate=115200, timeout=1.5):
         self.port = serial.Serial(port, baudrate=baudrate, timeout=timeout)
     
-    def send_data(self, data):
+    
+    def send_data(self, data=str()): #make sure this works as expected through the engine
+        self.data = data
         d = bytes(data, 'utf-8')
         self.port.write(d)
         rcv = self.port.read(16)
         return rcv
-##data = "SETLED 4 G"
+        ##data = "SETLED 4 G"
+        #LED data is sent as "SETLED [LED_NUM] [COLOR]" with color being R, G, B, Y 
+        
     def close(self):
         self.port.close()
-#LED data is sent as "SETLED [LED_NUM] [COLOR]" with color being R, G, B, Y 
-
+    
