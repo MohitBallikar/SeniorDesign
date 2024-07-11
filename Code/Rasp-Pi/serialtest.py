790833c69d27b9f5dcda7a40f48d5d51662a4c8a
Mohit <89613841+MohitBallikar@users.noreply.github.com>
2024-07-11 00:12:16 -0400

    Update serialtest.py, fixed/reordered

--- Code/Rasp-Pi/serialtest.py
+++ Code/Rasp-Pi/serialtest.py
@@ -2,13 +2,13 @@ import serial
 #USB0 is the commumication port
 #timeout is in seconds; ie 3 seconds here
 port = serial.Serial("/dev/ttyUSB0", baudrate=115200, timeout=1.5)
-data = "SETLED 4 G"
+data = "B2 SETLED 4 G"
 #LED data is sent as "SETLED [LED_NUM] [COLOR]" with color being R, G, B, Y 
 d = bytes(data,'utf-8')
+port.write(d)
 # if there are issues they can be troubleshooted from the below code
 while(True):
-    port.write(d)
-    rcv = port.read(16) #maybe set this to a value higher in case the buffer is misallocated
+    rcv = port.read(6) #maybe set this to a value higher in case the buffer is misallocated
     print("\r\Reply:" + repr(rcv))
     if(repr(rcv) == "b'Acknowledged'"):
         data = " "
