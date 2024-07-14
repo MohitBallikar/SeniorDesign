0f376df4be7c7c5238d510a72078419af73fba38
Mohit <89613841+MohitBallikar@users.noreply.github.com>
2024-07-14 04:02:57 -0400

    Update main.c; 9800 baud

--- Code/STM32/UART Commands With Buffer Clear/Core/Src - Identifier/main.c
+++ Code/STM32/UART Commands With Buffer Clear/Core/Src - Identifier/main.c
@@ -353,7 +353,7 @@ static void MX_SPI1_Init(void)
 static void MX_USART2_UART_Init(void)
 {
   huart2.Instance = USART2;
-  huart2.Init.BaudRate = 115200;
+  huart2.Init.BaudRate = 9800;
   huart2.Init.WordLength = UART_WORDLENGTH_8B;
   huart2.Init.StopBits = UART_STOPBITS_1;
   huart2.Init.Parity = UART_PARITY_NONE;
